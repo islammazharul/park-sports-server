044c3e72ab067d1ae171c5ba84031010fbd5fe61
islammazharul <islammazharul3645@gmail.com>
2023-06-09 10:44:00 +0600

    added the token generator

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    // console.log("from verify jwt", authorization);
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: "unauthorized access" })
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: "unauthorized access" })
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
+
 
 
 
@@ -30,11 +47,39 @@ async function run() {
         await client.connect();
 
         const sportsCollection = client.db("sportsDB").collection("sports");
+        const usersCollection = client.db("sportsDB").collection("users");
+
+
+
+        // jwt token
+
+        app.post("/jwt", (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
+
+        // Warning: use verifyJWT before using verifyAdmin
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            if (user?.role !== 'admin') {
+                return res.status(403).send({ error: true, message: 'forbidden message' })
+            }
+            next()
+        }
 
         // sports related api
         app.get("/sports", async (req, res) => {
             const result = await sportsCollection.find().toArray();
             // console.log("sports", result);
+            res.send(result)
+        })
+
+        app.get("/sports/:email", async (req, res) => {
+            const email = req.params.email;
+            const result = await sportsCollection.find({ email }).toArray()
             res.send(result)
         })
 
@@ -44,6 +89,7 @@ async function run() {
             const result = await sportsCollection.insertOne(sport);
             res.send(result)
         })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
